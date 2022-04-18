7e9b4a8b1b5f128566ccc59c697ec12b60941747
Giru <yagofarov.vadim@mail.ru>
2022-04-18 11:56:46 +0500

    fix genre

--- service/genre.py
+++ service/genre.py
@@ -1,11 +1,17 @@
 from dao.genre import GenreDAO
 
 
+class GenreNotFound(Exception):
+    pass
+
+
 class GenreService:
     def __init__(self, dao: GenreDAO):
         self.dao = dao
 
     def get_one(self, bid):
+        if bid == 0:
+            raise GenreNotFound
         return self.dao.get_one(bid)
 
     def get_all(self):
@@ -20,7 +26,7 @@ class GenreService:
     def partially_update(self, genre_d):
         genre = self.get_one(genre_d["id"])
         if "name" in genre_d:
-            genre.name = genre_d.get("name")
+            genre['name'] = genre_d.get("name")
         self.dao.update(genre)
 
     def delete(self, rid):
